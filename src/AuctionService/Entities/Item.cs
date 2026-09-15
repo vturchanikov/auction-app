fa2623ce4d52f35ff3c5c18dde21439a64d03b96
Vladimir <vladimir@malivision.com>
2026-09-15 16:25:14 -0300

    Initial Commit

---
+++ src/AuctionService/Entities/Item.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AuctionService.Entities;
+
+public class Item
+{
+    [MaxLength(36)]
+    public string Id { get; set; } = Guid.NewGuid().ToString();
+
+    [MaxLength(100)]
+    public required string Make { get; set; }
+
+    [MaxLength(100)]
+    public required string Model { get; set; }
+
+    [MaxLength(100)]
+    public required string Color { get; set; }
+
+    [MaxLength(2000)]
+    public required string Description { get; set; }
+
+    public int Year { get; set; }
+    public int Milage { get; set; }
+
+    [MaxLength(200)]
+    public required string ImageUrl { get; set; }
+    public Auction? Auction { get; set; } = null;
+}
